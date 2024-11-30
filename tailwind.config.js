e0ee5f972571d52800f02bc74790b97c3178f02d
Mohammad Shajalal <khanranu81@gmail.com>
2024-11-30 12:20:26 +0600

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'color-primary': '#589167',
+        'color-text': '#b2b2b2',
+        'color-secendary': ''
+      },
+      fontFamily : {
+        'font-roboto': 'Roboto'
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '30px',
+        md: '40px'
+      }
+    }
+  },
+  plugins: [],
+}
