da7ec8ccdfead9236412e27ddb7b3a4d9a5b38c3
Mohammad Shajalal <khanranu81@gmail.com>
2024-12-01 16:02:30 +0600

    blog sction create

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         'color-text': '#b2b2b2',
         'color-secendary': '#f5f4f0',
         'color-white-dark': '#808080',
+        'color-black-light': '#1a1a1a',
+        'color-black-ass': ''
       },
       fontFamily : {
         'font-roboto': 'Roboto'
